b2d77f3fe56f31e55a1c6148d5e09ba14360f412
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-01 13:38:28 +0100

    Made it more readable

--- 1.py
+++ 1.py
@@ -1,11 +1,5 @@
-import re
+from re import findall as a
+from operator import itemgetter as b
 
-def to_num(letters):
-    if letters.isdigit():
-        return letters
-    return str({'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9}[letters])
-
-sum = 0
-for line in open('input.txt').readlines():
-    matches = re.findall('(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))', line)
-    sum += int(str(to_num(matches[0])) + str(to_num(matches[-1])))
+c = 'one|two|three|four|five|six|seven|eight|nine'
+print(sum([int(''.join([x if x.isdigit() else str(c.split('|').index(x)+1) for x in b(0,-1)(a(f"(?=([1-9]|{c}))", l))])) for l in open('input.txt').readlines()]))
