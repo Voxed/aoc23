e3e07d4b9cc9598c7907641fc979f8d7fa411e10
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-02 09:35:37 +0100

    eliminated

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 from numpy import *
 v = array([list(dict([(ord(t), 0) for t in 'rgb'] + sorted([(ord(b[0]), int(a))
              for a, b in reshape(f.split()[2:], (-1, 2))], key=sum)).values()) for f in open('input.txt').readlines()])
-print(sum(arange(len(v))[(v-13 < range(3)).all(1)]+1), sum(v.prod(1)))
+print(sum(argwhere((v-13 < range(3)).all(1))+1), sum(v.prod(1)))
                               
