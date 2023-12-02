e88dcf09ab648eac1231aff309a09f7afb6850bc
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-02 08:43:19 +0100

    lil'

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
-import numpy as np
+import numpy as n
 from operator import itemgetter as z
 
-v = np.array([list(dict([(ord(t), 0) for t in 'rgb'] + sorted([(ord(b[0]), int(a))
-             for a, b in np.reshape(f.split()[2:], (-1, 2))], key=z(1))).values()) for f in open('input.txt').readlines()])
-print(sum(np.arange(len(v))[(v < (13, 14, 15)).all(1)]+1), sum(v.prod(1)))
+v = n.array([list(dict([(ord(t), 0) for t in 'rgb'] + sorted([(ord(b[0]), int(a))
+             for a, b in n.reshape(f.split()[2:], (-1, 2))], key=z(1))).values()) for f in open('inut.txt').readlines()])
+print(sum(n.arange(len(v))[(v < (13, 14, 15)).all(1)]+1), sum(v.prod(1)))
