ec5d5b44670027af21a4273d042bb666b374f4d8
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-02 10:14:59 +0100

    pop

--- 2.py
+++ 2.py
@@ -1,5 +1,4 @@
 from numpy import *
-v = array([list(dict([(ord(t), 0) for t in 'rgb'] + sorted([(ord(b[0]), int(a))
-             for a, b in reshape(f.split()[2:], (-1, 2))], key=sum)).values()) for f in open('input.txt').readlines()])
+v = array([list(dict(list(ndindex(3, 1)) + sorted([(ord(b[0]) % 3, int(a))
+                                                  for a, b in reshape(f.split()[2:], (-1, 2))], key=sum)).values()) for f in open('input.txt').readlines()])
 print(sum(argwhere((v-13 < range(3)).all(1))+1), sum(v.prod(1)))
-                              
