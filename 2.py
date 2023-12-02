3073905ce17dc455475e9eaee3bff96a644fae2b
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-02 08:42:15 +0100

    hoho

--- 2.py
+++ 2.py
@@ -3,5 +3,4 @@ from operator import itemgetter as z
 
 v = np.array([list(dict([(ord(t), 0) for t in 'rgb'] + sorted([(ord(b[0]), int(a))
              for a, b in np.reshape(f.split()[2:], (-1, 2))], key=z(1))).values()) for f in open('input.txt').readlines()])
-print(sum(np.arange(1, len(v)+1)[(v <= (12, 13, 14)).all(axis=1)]))
-print(sum(v.prod(axis=1)))
+print(sum(np.arange(len(v))[(v < (13, 14, 15)).all(1)]+1), sum(v.prod(1)))
