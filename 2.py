4394c337b7644054238e64a3845308637c60cd73
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-02 12:27:25 +0100

    nah

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
 from numpy import *
-v = array([[*dict([*ndindex(3, 1), *sorted([(ord(b[0]) % 3, int(a)) for a, b in reshape(f.split()[2:], (-1, 2))])]).values()] for f in open('input.txt')])
+v = array([[*dict([*ndindex(3, 1), *sorted([(ord(b[0]) % 3, int(a))
+          for a, b in reshape(f.split()[2:], (-1, 2))])]).values()] for f in open('input.txt')])
 print(sum(argwhere((v-13 < range(3)).all(1))+1), sum(v.prod(1)))
