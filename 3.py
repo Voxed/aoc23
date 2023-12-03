2282f54b3ec2f7fa3150c8f705e91f72be3c5df1
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 07:32:09 +0100

    More verbose names

--- 3.py
+++ 3.py
@@ -1,18 +1,16 @@
 from numpy import *
-map = pad(array([[*f.strip()] for f in open("input.txt")]), 1, constant_values='.')
-symbols = ~char.isdigit(map) & (map != '.')
-gear_mask = map == '*'
-gear_count = zeros(map.shape)
-gear_val = gear_count + 1
-
-z = char.isdigit(map)
+m = pad([[*f.strip()] for f in open("input.txt")], 1, constant_values='.')
+s = ~char.isdigit(m) & (m != '.')
+c = zeros(m.shape)
+v = (m == '*').astype(int)
+z = char.isdigit(m)
 p = column_stack((column_stack(nonzero(~z[1:, :-1] & z[1:, 1:]))+1, column_stack(nonzero(z[1:, :-1] & ~z[1:, 1:]))+1))
+
 sum = 0
 for sx, sy, ex, ey in p:
-    n = int(''.join(map[sx:ex+1, sy:ey].flatten()))
-    if symbols[sx-1:ex+2,sy-1:ey+1].any():
-        sum += n
-    gear_count[sx-1:ex+2,sy-1:ey+1][gear_mask[sx-1:ex+2,sy-1:ey+1]] += 1
-    gear_val[sx-1:ex+2,sy-1:ey+1][gear_mask[sx-1:ex+2,sy-1:ey+1]] *= n
+    n = int(''.join(m[sx:ex+1, sy:ey].flatten()))
+    sum += s[sx-1:ex+2,sy-1:ey+1].any()*n
+    c[sx-1:ex+2,sy-1:ey+1][v[sx-1:ex+2,sy-1:ey+1] > 0] += 1
+    v[sx-1:ex+2,sy-1:ey+1][v[sx-1:ex+2,sy-1:ey+1] > 0] *= n
 
-print(sum, gear_val[gear_count == 2].sum())
+print(sum, v[c == 2].sum())
