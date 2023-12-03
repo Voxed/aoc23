0ad89a5de521052cad6ebb22c8743c5617b99332
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 09:05:07 +0100

    Okay

--- 3.py
+++ 3.py
@@ -1,7 +1,6 @@
 from numpy import *
 m = pad([[*f.strip()] for f in open("input.txt")], 1, constant_values='.')
-z = char.isdigit(m)
-v, o, x, e, d = ~z & (m != '.'), (w := column_stack)((w((g := nonzero)(z & ~(
+v, o, x, e, d = ~(z := char.isdigit(m)) & (m != '.'), (w := column_stack)((w((g := nonzero)(z & ~(
     t := roll(z, 1, 1)))), w(g(~z & t)))), zeros(m.shape), array(m == '*', int), 0
 for h, j, k, l in o:
     n = int(''.join(m[h:k+1, j:l].flatten()))
