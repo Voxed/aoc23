813d85e6a09cf40723e19ab424178198194879f2
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 07:58:04 +0100

    2

--- 3.py
+++ 3.py
@@ -13,7 +13,7 @@ for sx, sy, ex, ey in p:
     q = full(v.shape, False)
     q[sx-2:ex+1,sy-1:ey+1] = True
 
-    r += s[sx-2:ex+1,sy-1:ey+1].any()*n
+    r += s[q].any()*n
     c += 1 * (q & v > 0)
     v *= q*n + ~q*1
 
