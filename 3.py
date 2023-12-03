0f4418963909c9dfaf28fb8fb42c14e3db71bc4c
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 08:30:02 +0100

    : - )

--- 3.py
+++ 3.py
@@ -4,14 +4,15 @@ s = ~char.isdigit(m) & (m != '.')
 c = zeros(m.shape)
 v = (m == '*').astype(int)
 z = char.isdigit(m)
-p = column_stack((column_stack(nonzero(~z[:, :-1] & z[:, 1:]))+1, column_stack(nonzero(z[:, :-1] & ~z[:, 1:]))+1))
+p = column_stack((column_stack(nonzero(z & ~(t := roll(z, 1, 1)))), column_stack(nonzero(~z & t))))
 r = 0
 
-for sx, sy, ex, ey in p:
-    n = int(''.join(m[sx-1:ex, sy:ey].flatten()))
-    (q := full(v.shape, False))[sx-2:ex+1,sy-1:ey+1] = True
+for h, j, k, l in p:
+    n = int(''.join(m[h:k+1, j:l].flatten()))
+    (q := full(v.shape, False))[h-1:k+2,j-1:l+1] = True
     r += s[q].any()*n
     c += 1 * (q & v > 0)
     v *= q*n + ~q*1
+
 
 print(r, v[c == 2].sum())
