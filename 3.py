333ac1c46af468aa7066451c1e3d875097e8f504
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 09:14:15 +0100

    Less

--- 3.py
+++ 3.py
@@ -4,8 +4,7 @@ g = nonzero
 m = pad([[*f.strip()] for f in open("input.txt")], 1, constant_values='.')
 z = char.isdigit(m)
 t = roll(z, 1, 1)
-v, o, x, e, d = ~z & (m != '.'), w((w(g(z & ~t)), w(
-    g(~z & t)))), zeros(m.shape), array(m == '*', int), 0
+v, o, x, e, d = ~z & (m != '.'), w((w(g(z & ~t)), w(g(~z & t)))), zeros(m.shape), array(m == '*', int), 0
 for h, j, k, l in o:
     n = int(''.join(m[h:k+1, j:l].flatten()))
     (q := full(e.shape, False))[h-1:k+2, j-1:l+1] = True
