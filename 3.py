4cf8caef894a47a7050b54ec82b9056733787575
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 07:39:28 +0100

    For a few bytes more

--- 3.py
+++ 3.py
@@ -4,13 +4,13 @@ s = ~char.isdigit(m) & (m != '.')
 c = zeros(m.shape)
 v = (m == '*').astype(int)
 z = char.isdigit(m)
-p = column_stack((column_stack(nonzero(~z[1:, :-1] & z[1:, 1:]))+1, column_stack(nonzero(z[1:, :-1] & ~z[1:, 1:]))+1))
+p = column_stack((column_stack(nonzero(~z[:, :-1] & z[:, 1:]))+1, column_stack(nonzero(z[:, :-1] & ~z[:, 1:]))+1))
+r = 0
 
-s = 0
 for sx, sy, ex, ey in p:
-    n = int(''.join(m[sx:ex+1, sy:ey].flatten()))
-    s += s[sx-1:ex+2,sy-1:ey+1].any()*n
-    c[sx-1:ex+2,sy-1:ey+1][v[sx-1:ex+2,sy-1:ey+1] > 0] += 1
-    v[sx-1:ex+2,sy-1:ey+1][v[sx-1:ex+2,sy-1:ey+1] > 0] *= n
+    n = int(''.join(m[sx-1:ex, sy:ey].flatten()))
+    r += s[sx-2:ex+1,sy-1:ey+1].any()*n
+    c[sx-2:ex+1,sy-1:ey+1][v[sx-2:ex+1,sy-1:ey+1] > 0] += 1
+    v[sx-2:ex+1,sy-1:ey+1][v[sx-2:ex+1,sy-1:ey+1] > 0] *= n
 
-print(s, v[c == 2].sum())
+print(r, v[c == 2].sum())
