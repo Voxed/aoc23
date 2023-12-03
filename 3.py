bb8d77c746d4d6207ebdb332cc0c9217860e2363
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 09:00:18 +0100

    egg :)

--- 3.py
+++ 3.py
@@ -1,16 +1,12 @@
 from numpy import *
 m = pad([[*f.strip()] for f in open("input.txt")], 1, constant_values='.')
 z = char.isdigit(m)
-s = ~z & (m != '.')
-p = (w := column_stack)(
-    (w((e := nonzero)(z & ~(t := roll(z, 1, 1)))), w(e(~z & t))))
-c = zeros(m.shape)
-v = array(m == '*', int)
-r = 0
-for h, j, k, l in p:
+v, o, x, e, d = ~z & (m != '.'), (w := column_stack)((w((g := nonzero)(z & ~(
+    t := roll(z, 1, 1)))), w(g(~z & t)))), zeros(m.shape), array(m == '*', int), 0
+for h, j, k, l in o:
     n = int(''.join(m[h:k+1, j:l].flatten()))
-    (q := full(v.shape, False))[h-1:k+2, j-1:l+1] = True
-    r += s[q].any()*n
-    c += 1 * (q & v > 0)
-    v *= q*n + ~q*1
-print(r, v[c == 2].sum())
+    (q := full(e.shape, False))[h-1:k+2, j-1:l+1] = True
+    d += v[q].any()*n
+    x += 1 * (q & e > 0)
+    e *= q*n + ~q*1
+print(d, e[x == 2].sum())
