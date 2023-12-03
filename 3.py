f24d8ff0c9c7b70d7efaddf566b53a333dcd3cc5
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 07:33:20 +0100

    Even more verbose

--- 3.py
+++ 3.py
@@ -6,11 +6,11 @@ v = (m == '*').astype(int)
 z = char.isdigit(m)
 p = column_stack((column_stack(nonzero(~z[1:, :-1] & z[1:, 1:]))+1, column_stack(nonzero(z[1:, :-1] & ~z[1:, 1:]))+1))
 
-sum = 0
+s = 0
 for sx, sy, ex, ey in p:
     n = int(''.join(m[sx:ex+1, sy:ey].flatten()))
-    sum += s[sx-1:ex+2,sy-1:ey+1].any()*n
+    s += s[sx-1:ex+2,sy-1:ey+1].any()*n
     c[sx-1:ex+2,sy-1:ey+1][v[sx-1:ex+2,sy-1:ey+1] > 0] += 1
     v[sx-1:ex+2,sy-1:ey+1][v[sx-1:ex+2,sy-1:ey+1] > 0] *= n
 
-print(sum, v[c == 2].sum())
+print(s, v[c == 2].sum())
