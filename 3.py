4b82041a081ab13d6ae869f36b8bd7844f8d79f1
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-03 08:34:34 +0100

    Readability is key

--- 3.py
+++ 3.py
@@ -1,18 +1,16 @@
 from numpy import *
 m = pad([[*f.strip()] for f in open("input.txt")], 1, constant_values='.')
-s = ~char.isdigit(m) & (m != '.')
+z = char.isdigit(m)
+s = ~z & (m != '.')
 c = zeros(m.shape)
 v = (m == '*').astype(int)
-z = char.isdigit(m)
-p = column_stack((column_stack(nonzero(z & ~(t := roll(z, 1, 1)))), column_stack(nonzero(~z & t))))
+p = (w := column_stack)(
+    (w((e := nonzero)(z & ~(t := roll(z, 1, 1)))), w(e(~z & t))))
 r = 0
-
 for h, j, k, l in p:
     n = int(''.join(m[h:k+1, j:l].flatten()))
-    (q := full(v.shape, False))[h-1:k+2,j-1:l+1] = True
+    (q := full(v.shape, False))[h-1:k+2, j-1:l+1] = True
     r += s[q].any()*n
     c += 1 * (q & v > 0)
     v *= q*n + ~q*1
-
-
 print(r, v[c == 2].sum())
