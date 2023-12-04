39e3787f7b0178868d1f5acd3a713bfed962c65a
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-04 18:36:25 +0100

    Cool

--- 4.py
+++ 4.py
@@ -1,8 +1,6 @@
 from numpy import *
 from itertools import *
 from functools import *
-
-c = array(list(zip_longest(*[[0]*(i+1) + [1]*len(a & b) for i, (a, b) in enumerate([[set([int(m)
-          for m in n.split() if m.isnumeric()]) for n in f.strip().split('|')] for f in open('input.txt')])], fillvalue=0)))
-print((1 << c.sum(0)-1).sum(), reduce(lambda a,
-      b: vstack([a, c.dot(a[-1])]), range(len(c)), [[1]*len(c)]).sum())
+p = enumerate([[set([int(m) for m in n.split()]) for n in f.strip().split(':')[1].split('|')] for f in open('input.txt')])
+c = array([*zip_longest(*[[0]*(i+1) + [1]*len(a & b) for i, (a, b) in p], fillvalue=0)])
+print((1 << c.sum(0)-1).sum(), reduce(lambda a, b: vstack([a, dot(c, a[-1])]), c, [[1]*len(c)]).sum())
