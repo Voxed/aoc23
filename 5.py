01f8687c5c4527aca08821a865503c1e712b8009
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-05 08:03:34 +0100

    cmt

--- 5.py
+++ 5.py
@@ -1,10 +1,12 @@
 from numpy import *
+# Parse input
 chunks = [r.strip().split('\n') for r in open('input.txt').read().split('\n\n')]
-objects_p2 = [('seed', p[0], p[0] + p[1]) for p in array(reshape(chunks[0][0].split()[1:], (-1, 2)), int64)]
 objects_p1 = [('seed', p, p + 1) for p in array(chunks[0][0].split()[1:], int64)]
+objects_p2 = [('seed', p[0], p[0] + p[1]) for p in array(reshape(chunks[0][0].split()[1:], (-1, 2)), int64)]
+
+# Decode convertors and name progression
 convertors = []
 name_progression = {}
-
 for raw_name, *raw_convertors in chunks[1:]:
     c_from, _, c_to = raw_name.split()[0].split('-')
     name_progression[c_from] = c_to
@@ -13,6 +15,7 @@ for raw_name, *raw_convertors in chunks[1:]:
         convertors.append((c_from, c_to, c_source, c_source+c_length, c_dest-c_source))
 
 def get_min_location(objects):
+    '''Calculate the minimum location of an object in objects'''
     min_location = float('inf')
     for o_state, o_start, o_end in objects:
         while o_state != 'location':
@@ -27,4 +30,5 @@ def get_min_location(objects):
         min_location = min(o_start, min_location)
     return min_location
 
+# Print answers
 print(get_min_location(objects_p1), get_min_location(objects_p2))
