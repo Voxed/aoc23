1928f375d79183a5716e04e1e1c3c4fcf53c47a9
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-06 07:34:27 +0100

    Dumb

--- 6.py
+++ 6.py
@@ -4,6 +4,6 @@ abc = lambda a, b, c: [(-b + r*(b**2 - 4*a*c)**0.5) / 2*a for r in [-1, 1]] # ro
 record_combinations = lambda p: floor((r := abc(-1, p[0], -p[1]-eps))[0]) - ceil(r[1]) + 1
 inp = [l[10:].strip() for l in open('input.txt')]
 print(
-    prod([*map(record_combinations, zip(*[map(int, l.split()) for l in inp]))]), 
+    prod(map(record_combinations, zip(*[map(int, l.split()) for l in inp]))), 
     record_combinations([int(l.replace(' ', '')) for l in inp])
 )
