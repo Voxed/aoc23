914909fa0a4f2d2fec06d0dd1c7a0964ff7df66a
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-06 07:30:32 +0100

    look ma, no numpy

--- 6.py
+++ 6.py
@@ -1,7 +1,9 @@
 from math import *
-from numpy import *
-record_combinations = lambda p: abs(floor((r := roots([-1, p[0], -p[1]-0.001]))[0]) - ceil(r[1]) + 1)
+eps = 0.001
+abc = lambda a, b, c: [(-b + r*(b**2 - 4*a*c)**0.5) / 2*a for r in [-1, 1]] # roots([a,b,c]) in numpy
+record_combinations = lambda p: floor((r := abc(-1, p[0], -p[1]-eps))[0]) - ceil(r[1]) + 1
+inp = [l[10:].strip() for l in open('input.txt')]
 print(
-    prod([*map(record_combinations, zip(*[map(int, l[10:].strip().split()) for l in open('input.txt')]))]), 
-    *map(record_combinations, [[int(l[10:].replace(' ', '').strip()) for l in open('input.txt')]])
+    prod([*map(record_combinations, zip(*[map(int, l.split()) for l in inp]))]), 
+    record_combinations([int(l.replace(' ', '')) for l in inp])
 )
