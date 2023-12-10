e03ae829e8fa7b63b98a4713d1e6fbcce393bb93
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-10 09:18:39 +0100

    Ugly but works

--- 10.py
+++ 10.py
@@ -1,57 +1,47 @@
 from numpy import *
 
-import sys
-sys.setrecursionlimit(100000)
+m = [[{
+    '|': [(0,1), (0,-1)],
+    '-': [(1,0), (-1,0)],
+    '7': [(-1, 0), (0, 1)],
+    'F': [(1, 0), (0, 1)],
+    'J': [(-1, 0), (0, -1)],
+    'L': [(1, 0), (0, -1)],
+    '.': [(0, 0)],
+    'S': [(1,1), (1, 0), (-1, 0), (0, 1), (0, -1)]
+    }[s] for s in l.strip()] for l in open('input.txt')]
 
-m = pad([[{
-    '|': set([(0,1), (0,-1)]),
-    '-': set([(1,0), (-1,0)]),
-    '7': set([(-1, 0), (0, 1)]),
-    'F': set([(1, 0), (0, 1)]),
-    'J': set([(-1, 0), (0, -1)]),
-    'L': set([(1, 0), (0, -1)]),
-    '.': set([(0, 0)]),
-    'S': set([(1, 1)])
-    }[s] for s in l.strip()] for l in open('input.txt')], pad_width=1, constant_values=(set([(0,0), (0,0)])))
+def search(x,y, done=[], steps = 1):
+    done = [(x,y)]
+    xd, yd = m[y][x][0]
+    x, y = x+xd, y+yd
+    steps += 1
+    while (x,y) not in done:
+        done += [(x, y)]
+        for xd, yd in set(m[y][x]):
+            if (x + xd, y + yd) not in done:
+                x, y = x + xd, y + yd
+                steps += 1
+                break
+    return steps, done
 
-minimum = {}
-
-def search(o, x,y, done=[], steps = 1):
-    for xl, yl in o:
-        if (x + xl, y + yl) not in done:
-            if (xl*-1, yl*-1) in m[y + yl][x + xl]:
-                if (x + xl, y + yl) in minimum:
-                    minimum[(x + xl, y + yl)] = min(minimum[(x + xl, y + yl)], steps) 
-                else:
-                    minimum[(x + xl, y + yl)] = steps
-                search(m[y + yl][x + xl], x + xl, y + yl, done = done + [(x + xl, y + yl)], steps = steps + 1)
-
+start_x, start_y = 0, 0
 for y in range(len(m)):
     for x in range(len(m[0])):
         o = m[y][x]
         if (1,1) in o:
-            search(set([(0,1), (1,0), (-1,0), (0, -1)]), x,y)
-            minimum[(x,y)] = 0
-            print(minimum)
+            m[y][x] = [d for d in o if (d[0]*-1, d[1]*-1) in m[y+d[1]][x+d[0]]]
+            start_x = x
+            start_y = y
 
-his = sorted([(minimum[k], k) for k in minimum])
+r, cycle = search(start_x,start_y)
 
-print(his[-1][0])
 c = 0
 for y in range(len(m)):
     inside = False
     u = False
     for x in range(len(m[0])):
-        if (x, y) in minimum:
-            if (1,1) in m[y][x]:
-                if (-1, 0) in m[y][x+1]:
-                    m[y][x].add((1,0))
-                if (1, 0) in m[y][x-1]:
-                    m[y][x].add((-1,0))
-                if (0, -1) in m[y+1][x]:
-                    m[y][x].add((0,1))
-                if (0, 1) in m[y-1][x]:
-                    m[y][x].add((0,-1))
+        if (x, y) in cycle:
             if (1, 0) not in m[y][x] or (-1,0) not in m[y][x]:
                 if (0,1) in m[y][x] and (0,-1) in m[y][x]:
                     inside = not inside
@@ -61,11 +51,6 @@ for y in range(len(m)):
                             inside = not inside
                     else:
                         u = (0, 1) in m[y][x]
-            print('#', end='')
         elif inside:
             c += 1
-            print('I', end='')
-        else:
-            print('O', end='')
-    print('')
-print(c)
+print(int(ceil(r/2)), c)
