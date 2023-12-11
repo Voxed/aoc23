746b96d75fcadfc56f1be366811aec8370c64720
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-11 08:17:55 +0100

    Blah

--- 11.py
+++ 11.py
@@ -8,6 +8,6 @@ for d in (2, 1000000):
     s = 0.0
     p = [*combinations(vstack(nonzero(m == '#')).transpose(), 2)]
     for y1, x1, y2, x2 in hstack([amin(p, 1), amax(p, 1)]):
-        s += er[y1:y2].sum()*d + (~er)[y1:y2].sum() + ec[x1:x2].sum()*d + (~ec)[x1:x2].sum()
+        s += (er[y1:y2].sum() + ec[x1:x2].sum())*d + (~ec)[x1:x2].sum() + (~ec)[x1:x2].sum()
     distances.append(s)
 print(*distances)
