8caafa057009b5b592684de4fff47ebf5f280a37
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-11 08:23:35 +0100

    Kay

--- 11.py
+++ 11.py
@@ -1,11 +1,9 @@
 from numpy import *
-from itertools import * 
+from itertools import *
 m = array([[*l.strip()] for l in open('input.txt').readlines()])
 er, ec = all(m == '.', 1), all(m.transpose() == '.', 1)
 for d in (2, 1000000):
-    s = 0.0
     p = [*combinations(vstack(nonzero(m == '#')).transpose(), 2)]
-    for y1, x1, y2, x2 in hstack([amin(p, 1), amax(p, 1)]):
-        s += (er[y1:y2].sum() + ec[x1:x2].sum())*d + (~er)[y1:y2].sum() + (~ec)[x1:x2].sum()
-    print(int(s), end=' ')
+    print(int(sum([*map(float, [(er[y1:y2].sum() + ec[x1:x2].sum())*d + (~er)[y1:y2].sum() + (~ec)[x1:x2].sum()
+                                for y1, x1, y2, x2 in hstack([amin(p, 1), amax(p, 1)])])])), end=' ')
 print('')
