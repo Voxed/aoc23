755be9be5f1d134637b29599e662c92b0b16440a
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-12 15:33:19 +0100

    nump

--- 12.py
+++ 12.py
@@ -1,39 +1,11 @@
 from numpy import *
 import functools
 
-def segscan(arr, reset_on): # not actually segscan
-    rs = 0
-    rs2 = 0
-    finsums = []
-    sums = []
-    for e in arr:
-        if e == reset_on:
-            if rs != 0:
-                sums.append(rs)
-                rs = 0
-            if rs2 != 0:
-                sums.append(rs2)
-                rs2 = 0
-            if len(sums) != 0:
-                finsums.append(tuple(sums))
-                sums = []
-        if e == 1:
-            rs += e
-            if rs2 != 0:
-                sums.append(rs2)
-                rs2 = 0
-        if e == -1:
-            rs2 -= 1 
-            if rs != 0:
-                sums.append(rs)
-                rs = 0
-    if rs != 0:
-        sums.append(rs)
-    if rs2 != 0:
-        sums.append(rs2)
-    if len(sums) > 0:
-        finsums.append(tuple(sums))
-    return tuple(finsums)
+
+def scan(arr):
+    q = pad(arr, (0, 1)) != pad(arr, (1, 0))
+    r = array([t.sum() for t in split(arr, where(q)[0])])
+    return tuple([tuple(x[1:]) for x in split(r, where(r == 0)[0]) if x.shape != (0,)][:-1])
 
 @functools.cache
 def sols(comb, val):
@@ -88,7 +60,7 @@ def sols(comb, val):
     return rest + skip
 
 
-i = [(segscan(([{'.': 0, '?': -1, '#': 1}[c] for c in l.split()[0]] + [-1])*4 + [{'.': 0, '?': -1, '#': 1}[c] for c in l.split()[0]], 0), tuple(map(int, l.split()[1].strip().split(',')))*5) for l in open('input.txt')]
+i = [(scan(([{'.': 0, '?': -1, '#': 1}[c] for c in l.split()[0]] + [-1])*4 + [{'.': 0, '?': -1, '#': 1}[c] for c in l.split()[0]]), tuple(map(int, l.split()[1].strip().split(',')))*5) for l in open('input.txt')]
 s = 0
 q = len(i)
 t = 0
