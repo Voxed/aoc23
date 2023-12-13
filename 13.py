d80ddf3da7ca90e84fd08740e3cf62ad1967f6ce
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-13 08:32:36 +0100

    a bit but line 7 can probably be shortened a bit

--- 13.py
+++ 13.py
@@ -4,7 +4,7 @@ for p in [array([*map(list, p.split('\n'))]) for p in open('input.txt').read().s
     for c, p in (100, p), (1, p.transpose()):
         for r in range(0, p.shape[0]):
             h = min(r, p.shape[0] - r)
-            m = p[r-1::-1][:h] != p[r:][:h]
+            m = p[r-1::-1][:h] != p[r:r+h]
             s += ~m.any() * r * c
             t += (m.sum() == 1) * r * c
 print(s, t)
