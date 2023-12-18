f36614359d5eabba7658c0021d41aa76b7d4f72b
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-18 14:23:53 +0100

    achunk

--- 18.py
+++ 18.py
@@ -5,5 +5,4 @@ i = [(a, int(b), int(c[-7:-2], 16), int(c[-2], 16)) for a,b,c in [l.strip().spli
 s, t = array([[0, 1], [1, 0], [0, -1], [-1, 0]]), array([0, 0])
 print(*[int(Polygon(u).buffer(0.5, join_style=2).area) for u in [
     reduce(lambda u, i: u + [u[-1] + s['RDLU'.index(i[0])]*i[1]], i, [t]), 
-    reduce(lambda u, i: u + [u[-1] + s[i[3]]*i[2]], i, [t])
-]])
+    reduce(lambda u, i: u + [u[-1] + s[i[3]]*i[2]], i, [t])]])
