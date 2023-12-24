e21b224501ec8fd53c5c89938e6f488a4e575535
Voxed <petter.blomkvist.pb@gmail.com>
2023-12-24 11:32:43 +0000

    Boo

--- 24.py
+++ 24.py
@@ -1,30 +1,25 @@
+from sympy import *
 from numpy import *
 from itertools import *
+from sympy.abc import s, t, u, v, w, x, y, z, h, j, k
 
 l = array([l.strip().replace(' @ ', ' , ').split(', ')
           for l in open('input.txt')], int)
 
-answer_1 = 0
-
+# Part 1
+a1 = 0
 for l1, l2 in combinations(l, 2):
     try:
-        s, t = linalg.solve(
-            array([l1[3:5], -l2[3:5]]).T, l2[:2] - l1[:2])
-        x, y, z = l1[:3] + l1[3:6]*s
-        if s >= 0 and t >= 0:
-            b = (200000000000000.0, 400000000000000.0)
-            if b[0] <= x <= b[1] and b[0] <= y <= b[1]:
-                answer_1 += 1
+        r = linalg.solve(array([l1[3:5], -l2[3:5]]).T, l2[:2] - l1[:2])
+        i = l1[:2] + l1[3:5]*r[0]
+        a1 += (r[0] >= 0 <= r[1]) * all((i >= 2*10**14) & (i <= 4*10**14))
     except linalg.LinAlgError:
         pass
 
-# I can't do it myself :(
-from sympy.abc import s, t, u, v, w, x, y, z, h, j, k
-from sympy import *
-
-answer_2 = sum(solve(
+# Part 2
+a2 = sum(solve(
     (l[:, :3][:5] - [x, y, z] +
      array([[s, t, u, v, w]]).T * (l[:, 3:][:5] - [h, j, k])
      ).flatten(), (s, t, u, v, w, h, j, k, x, y, z))[0][-3:])
 
-print(answer_1, answer_2)
+print(a1, a2)
