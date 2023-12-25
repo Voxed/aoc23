50e0c959ba7057e3415d75cf6f0c54b872cf7b7a
Voxed <voxedg@gmail.com>
2023-12-25 15:07:55 +0100

    general

--- 25.py
+++ 25.py
@@ -1,12 +1,7 @@
 from networkx import *
+from math import *
 
-G = Graph(dict([(a, b.split()) for a, b in [l.strip().split(': ') for l in open('input.txt')]]))
-
-# I drew the graph :^)
-G.remove_edge('sph', 'rkh')
-G.remove_edge('hrs', 'mnf')
-G.remove_edge('nnl', 'kpc')
-
-for i, c in enumerate(connected_components(G)):
-    print(f"SiZZe: {len(c)}")
-
+print(prod(map(len, community.greedy_modularity_communities(
+    Graph(dict([(a, b.split()) for a, b in [l.strip().split(': ')
+          for l in open('input.txt')]])),
+    best_n=2))))
