4190294d79f32469b704950b51f08c7fcd680673
Voxed <voxedg@gmail.com>
2023-12-25 11:17:47 +0100

    Path of least resistance

---
+++ 25.py
@@ -0,0 +1,15 @@
+from numpy import *
+from collections import *
+from networkx import *
+from matplotlib import pyplot as plt
+
+G = Graph(dict([(a, b.split()) for a, b in [l.strip().split(': ') for l in open('input.txt')]]))
+
+# I drew the graph :^)
+G.remove_edge('sph', 'rkh')
+G.remove_edge('hrs', 'mnf')
+G.remove_edge('nnl', 'kpc')
+
+for i, c in enumerate(connected_components(G)):
+    print(f"SiZZe: {len(c)}")
+
